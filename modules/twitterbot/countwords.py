57decacff6bf25bdd15d34ec772ceda3bff11107
Elias <43931318+iampickle@users.noreply.github.com>
2023-08-15 16:22:17 +0200

    update word count

--- modules/twitterbot/countwords.py
+++ modules/twitterbot/countwords.py
@@ -3,7 +3,7 @@ import numpy as np
 from datetime import timedelta
 import matplotlib.pyplot as plt
 import uuid
-from ..twitter import *
+#from ..twitter import *
 import os
 
 def countsaidwords(workdir, channel):
@@ -30,21 +30,27 @@ def countsaidwords(workdir, channel):
 
     datesarray = []
     valuesarray = []
-    last = 0
+    """ last = 0
     for i in range(0, int(farray.max()), 10):
         count = ((last*60 < nparray) & (nparray < i*60)).sum()
         valuesarray.append(count)
         s = str(timedelta(minutes=i)).split(':')
         s = f'{s[0]}:{s[1]}'
         datesarray.append(s)
-        last = i
+        last = i """
     plt.style.use('dark_background')
-    plt.plot(datesarray,valuesarray)   
-    plt.tick_params(axis='x', labelrotation=90)
-    plt.savefig(filename)
+    plt.hist(farray, bins=120)
+    plt.title(f'Word-Count over Time:{channel}')
+    plt.ylabel('Words')
+    plt.xlabel('Total')
+    plt.show()
+    """ plt.plot(datesarray,valuesarray)   
+    plt.tick_params(axis='x', labelrotation=90)"""
+    plt.savefig(filename) 
     tweet_pic(filename, f"chart of word count over stream from: {channel}")
 
     """ plt.hist(nparray, bins=300)
     plt.gca().set(title='Frequency Histogram', ylabel='Frequency')
     plt.show() """
     
+countsaidwords('/Users/eliasmaurer/Develope/twitchbot/', 'papaplatte')    
