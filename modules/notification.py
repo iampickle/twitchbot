920bdd55d105fe02d94152fd54c4abf89e0fd1f2
iampickle <commandmastermail@gmail.com>
2023-09-02 13:51:07 +0200

    toggelable notifications

--- modules/notification.py
+++ modules/notification.py
@@ -8,18 +8,23 @@ from logbook import Logger, StreamHandler
 class notification:
 
     def __init__(self):
-        pass
-        self.Authtoken = os.environ.get("Authorization-IFTTT")
-        self.event = os.environ.get("event")
-        self.user = ''
+        if os.environ.get("Authorization-IFTTT"):
+            self.Authtoken = os.environ.get("Authorization-IFTTT")
+            self.event = os.environ.get("event")
+            self.user = ''
+            self.check=True
+        else:
+            self.check=False
+            
 
     def message(self, message):
-        log = Logger(self.user)
-        report = {}
-        report["value1"] = message
-        try:
-            requests.post(f"https://maker.ifttt.com/trigger/{self.event}/with/key/{self.Authtoken}", data=report)
-        except Exception as e:
-            print(f"notification could not be send: {e}")
+        if self.check == True:
+            log = Logger(self.user)
+            report = {}
+            report["value1"] = message
+            try:
+                requests.post(f"https://maker.ifttt.com/trigger/{self.event}/with/key/{self.Authtoken}", data=report)
+            except Exception as e:
+                print(f"notification could not be send: {e}")
 
-        log.info(f'📨 send message:"{message}", to event:"{self.event}"')
+            log.info(f'📨 send message:"{message}", to event:"{self.event}"')
