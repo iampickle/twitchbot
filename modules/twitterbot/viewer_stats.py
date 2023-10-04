bb3b04f7200f9f7c4450d6992f646ec520010892
iampickle <commandmastermail@gmail.com>
2023-10-04 14:31:46 +0200

    add simple category labels in text form

--- modules/twitterbot/viewer_stats.py
+++ modules/twitterbot/viewer_stats.py
@@ -9,6 +9,8 @@ import time
 
 
 def collect_data(token, stime, workdir, channel):
+    categorylegend = []
+    legendcount = 0
     url = 'https://api.twitch.tv/helix/streams?user_login=' + channel
     client_id=os.environ.get("Client-ID-Twitch")
     API_HEADERS = {
@@ -25,12 +27,16 @@ def collect_data(token, stime, workdir, channel):
     y_values = []
     change_title = []
     old_title = get_data('game_name')
+    
+    categorylegend.append(f'start: {old_title}\r')
     while True:
             try:
                 viewer = get_data('viewer_count')
                 now_title = get_data('game_name')
                 if now_title != old_title:
+                    legendcount += 1
                     old_title = now_title
+                    categorylegend.append(f'{str(legendcount)}: {now_title}\r')
                     print(now_title)
                     change_title.append(datetime.datetime.now())
             except:
@@ -62,5 +68,5 @@ def collect_data(token, stime, workdir, channel):
     plt.savefig(filename)
     plt.legend()
     plt.close()
-    tweet_pic(filename, f"chart of viewercount over stream from: {channel}")
+    tweet_pic(filename, f"chart of viewercount over stream from: {channel}", text=''.join(categorylegend))
     #os.remove(filename)
