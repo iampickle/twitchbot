326d7af9f83d6a7809085e2ca8e740623eac04a4
iampickle <commandmastermail@gmail.com>
2023-10-17 11:19:21 +0200

    right working dir and import at load

--- modules/twitterbot/countwords.py
+++ modules/twitterbot/countwords.py
@@ -1,13 +1,13 @@
 from ..twitter import *
-
+import json
+import numpy as np
+from datetime import timedelta
+import uuid
+import os
+import matplotlib.pyplot as plt
 
 def countsaidwords(results, workdir, channel):
-    import json
-    import numpy as np
-    from datetime import timedelta
-    import uuid
-    import os
-    import matplotlib.pyplot as plt
+    workdir = os.path.join(workdir, 'analytics/')
 
     name = str(uuid.uuid4())+'.png'
     filename = os.path.join(workdir, name)
