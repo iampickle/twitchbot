b515dcf273dbf01847fe734eed8cd9508af6c0c3
iampickle <commandmastermail@gmail.com>
2023-11-12 22:40:32 +0100

    text

--- modules/tiktok/upload.py
+++ modules/tiktok/upload.py
@@ -7,5 +7,5 @@ import subprocess
 cookies = os.environ.get("session-cookies")
 
 def tiktok_upload(channel, date, videopath):
-    subprocess.call(['tiktok-uploader', '-v', videopath, '-d', f'Zusammen fassung von {channel} am {date}', '-c', cookies], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print('done!')
+    subprocess.call(['tiktok-uploader', '-v', videopath, '-d', f'Zusammenfassung von {channel} am {date} #foryou #foryoupage #fyp #streaming #twitch #hightlight #{channel} #{channel}twitch #funny #germantwitch #lol', '-c', cookies], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f'done with uploading {channel} to tiktok!')
