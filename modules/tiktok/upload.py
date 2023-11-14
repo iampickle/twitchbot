89bb065e522eb3232af54cf8e14c98afcd6c6269
iampickle <commandmastermail@gmail.com>
2023-11-14 13:10:30 +0100

    error handeling

--- modules/tiktok/upload.py
+++ modules/tiktok/upload.py
@@ -7,5 +7,8 @@ import subprocess
 cookies = os.environ.get("session-cookies")
 
 def tiktok_upload(channel, date, videopath):
-    subprocess.call(['tiktok-uploader', '-v', videopath, '-d', f'Zusammenfassung von {channel} am {date} #foryou #foryoupage #fyp #streaming #twitch #hightlight #{channel} #{channel}twitch #funny #germantwitch #lol', '-c', cookies], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print(f'done with uploading {channel} to tiktok!')
+    try:
+        subprocess.call(['tiktok-uploader', '-v', videopath, '-d', f'Zusammenfassung von {channel} am {date} #foryou #foryoupage #fyp #streaming #twitch #hightlight #{channel} #{channel}twitch #funny #germantwitch #lol', '-c', cookies], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print(f'done with uploading {channel} to tiktok!')
+    except Exception as e:
+        print(f'failed tiktok upload of {channel} for reason: {e}')
