7a5c856d84bf69f26791cff96f56ba1a64f7e27a
iampickle <commandmastermail@gmail.com>
2024-03-16 17:37:04 +0100

    switch app push messaging to vybit

--- modules/notification.py
+++ modules/notification.py
@@ -1,30 +1,24 @@
+from logbook import Logger, StreamHandler
+import os
 import requests
 from dotenv import load_dotenv
 load_dotenv()
-import os
-from logbook import Logger, StreamHandler
 
 
 class notification:
 
     def __init__(self):
-        if os.environ.get("Authorization-IFTTT"):
-            self.Authtoken = os.environ.get("Authorization-IFTTT")
-            self.event = os.environ.get("event")
-            self.user = ''
-            self.check=True
-        else:
-            self.check=False
-            
+        self.user = ''
+        self.url = os.environ.get("message-url")
 
     def message(self, message):
-        if self.check == True:
-            log = Logger(self.user)
-            report = {}
-            report["value1"] = message
-            try:
-                requests.post(f"https://maker.ifttt.com/trigger/{self.event}/with/key/{self.Authtoken}", data=report)
-            except Exception as e:
-                print(f"notification could not be send: {e}")
+        log = Logger(self.user)
+        json = {"message" : message,
+                "linkUrl" : ''
+        }
+        try:
+            requests.post(self.url, json=json, headers={ "Content-Type" : "application/json" })
+        except Exception as e:
+            print(f"notification could not be send: {e}")
 
-            log.info(f'📨 send message:"{message}", to event:"{self.event}"')
+        log.info(f'📨 send message:"{message}"')
